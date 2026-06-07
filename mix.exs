72b2af2eec60104fa21d917abe08b026cefad775
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2026-06-07 16:57:08 +0200

    fix: compile on phoenix_live_view 1.2.0-rc
    - Remove stray closing brace in the theme dropdown of
      templates/layout/live.html.heex. LiveView 1.1's tokenizer silently
      tolerated it; 1.2's stricter parser raises a syntax error.
    - Require phoenix_live_view ~> 1.2.0-rc.

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule PhoenixStorybook.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.8.1"},
-      {:phoenix_live_view, "~> 1.1.0"},
+      {:phoenix_live_view, "~> 1.2.0-rc"},
       {:phoenix_html_helpers, "~> 1.0"},
       {:phoenix_view, "~> 2.0"},
       {:makeup_eex, "~> 2.0.2"},
